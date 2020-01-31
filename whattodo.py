41f5e3b4d906c2953b2f714484ce976a3160753d
mbasto <mario.basto@prozis.com>
2020-01-31 10:55:31 +0000

    python program

---
+++ whattodo.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+#coding=utf-8
+
+import sys
+import os
+import random
+import time
+
+file = sys.argv[1] if len(sys.argv) > 1 else "todo"
+print(file)
+path = "{}/{}.txt".format(os.path.expanduser(os.path.dirname(os.path.abspath(__file__))), file)
+
+with open(path) as f:
+    todo = f.read().splitlines()
+
+try:
+    i = 1
+    rand = random.randint(0, len(todo) - 1)
+    start_time = time.time()
+
+    while True:
+        input("{}- {}".format(i,todo[rand]))
+
+        print(time.strftime("%H:%M:%S",time.gmtime(time.time() - start_time)))
+
+        i += 1
+        rand = random.randint(0, len(todo) - 1)
+        start_time = time.time()
+
+except KeyboardInterrupt:
+    print()
